0b08db1e6c33e1449a1f3e920cfb9529b115e489
ruby_07 <ruby_07@swzs-iMac.local>
2024-03-05 14:30:56 +0800

    exercises

--- Simple_Ruby/Exercise1.rb
+++ Simple_Ruby/Exercise1.rb
@@ -9,5 +9,7 @@ def calculate_age(birth_year)
   current_year = 2024
   age = current_year - birth_year
 
-  p ("Birth year is #{birth_year} and age is #{age}")
+  print ("Birth year is #{birth_year} and age is #{age}\n")
 end
+
+p calculate_age(2000)
