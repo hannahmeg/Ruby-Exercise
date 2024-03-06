59ec54951902796f658f91a186b659bfe68a739d
ruby_07 <ruby_07@swzs-iMac.lan>
2024-03-06 11:06:40 +0800

    exercise

--- Simple_Ruby/Exercise7.rb
+++ Simple_Ruby/Exercise7.rb
@@ -1,9 +1,8 @@
 # Write a Ruby program that takes a temperature in Celsius as input,
 # converts it to Fahrenheit, and prints the result.
 
-def make_fahrenheit(celcius)
-  fahrenheit = (celcius * 9/5) + 32
-  print ("The fahrenheit of #{celcius}°C is #{fahrenheit}°F.\n")
-end
+puts "Input celcius:"
+celcius = gets.chomp.to_i
+fahrenheit = (celcius * 9/5) + 32
+print ("The fahrenheit of #{celcius}°C is #{fahrenheit}°F.\n")
 
-make_fahrenheit(10)
