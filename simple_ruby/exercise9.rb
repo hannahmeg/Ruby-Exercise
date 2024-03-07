95a1cacce2a1b17f8fc331592be3fa0020d98591
ruby_07 <ruby_07@swzs-iMac.local>
2024-03-07 14:37:38 +0800

    exercise

--- simple_ruby/exercise9.rb
+++ simple_ruby/exercise9.rb
@@ -4,17 +4,17 @@
 
 # script code
 puts "Enter year:"
-year = gets.chomp
-  if year%4 == 0
+year = gets.chomp.to_i
+  if year % 4 == 0 || year % 100 == 0 || year % 400 == 0
     print ("The year #{year} is a leap year.\n")
-  else
+    else
     print ("The year #{year} is not a leap year.\n")
   end
 
 
 # method code
 # def is_leap_year? (year)
-#   year % 4 == 0
+#   year % 4 == 0 || year % 100 == 0 || year % 400 == 0
 # end
 #
 # puts "Enter year:"
