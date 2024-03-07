c8ab45ab88b004c068d0c1261b5d3a755aeb3342
ruby_07 <ruby_07@swzs-iMac.local>
2024-03-07 16:29:22 +0800

    Check Temperature

--- array/exercise2.rb
+++ array/exercise2.rb
@@ -1,3 +1,18 @@
 # Create a Ruby script that takes an array of daily temperatures.
 # For each day, output "High" if the temperature is above 35 degrees Celsius,
 # "Moderate" if it's between 20 and 35 degrees, and "Low" if it's below 20 degrees.
+
+def check_temp(daily_temps = [])
+
+  daily_temps.map do |daily_temp|
+    if daily_temp > 35
+      daily_temp = "High"
+    elsif daily_temp >= 20 && daily_temp <= 35
+      daily_temp = "Moderate"
+    elsif daily_temp < 20
+      daily_temp = "Low"
+    end
+  end
+end
+
+p check_temp([20, 30, 10,50])
