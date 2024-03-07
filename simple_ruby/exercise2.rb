3a936f894294e2f0b161c838b98057270c11a026
ruby_07 <ruby_07@swzs-iMac.local>
2024-03-07 13:35:46 +0800

    exercise

--- simple_ruby/exercise2.rb
+++ simple_ruby/exercise2.rb
@@ -4,6 +4,7 @@
 # Perform addition, subtraction, multiplication, and division on the two numbers.
 # Print a message that includes the original numbers and the results of each operation.
 
+#script code
 puts "Input the first digit:"
 a = gets.chomp.to_i
 puts "Input the second digit:"
@@ -17,4 +18,18 @@ quot = a/b
 print ("The first number is #{a} and the second number is #{b}. The sum is #{sum}, difference is #{diff}, product is #{product}, and the quotient is #{quot}.")
 
 
-
+# # method code
+# def calculate(a, b)
+#   sum = a+b
+#   diff = a-b
+#   product = a*b
+#   quot = a/b.to_f
+#
+#   puts ("The first number is #{a} and the second number is #{b}. The sum is #{sum}, difference is #{diff}, product is #{product}, and the quotient is #{quot}.")
+# end
+#
+# puts "Input the first digit:"
+# a = gets.chomp.to_i
+# puts "Input the second digit:"
+# b = gets.chomp.to_i
+# calculate(a,b)
