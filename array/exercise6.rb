4187fe5dc641d9a6d4c9ae8756dd8f887571b91d
hannahmeg <hannah_arcega@yahoo.com>
2024-03-08 11:33:29 +0800

    Checking Stock Quantities

--- array/exercise6.rb
+++ array/exercise6.rb
@@ -2,3 +2,23 @@
 # For each product, if the quantity is greater than 50, mark it as
 # "In Stock"; otherwise, mark it as "Low Stock."
 
+def check_stock
+
+  puts "Input product quantity, press enter if none follows:"
+  quantities = []
+  while true
+    input = gets.chomp
+    break if input.empty?
+    quantities << input.to_i
+  end
+
+  quantities.map do |quantity|
+    if quantity > 50
+      score = "In Stock"
+    else
+      score = "Low Stock"
+    end
+  end
+end
+
+p check_stock
