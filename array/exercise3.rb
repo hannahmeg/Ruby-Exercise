0b860e158cb27115381be32aef15f18491c3c9f4
hannahmeg <hannah_arcega@yahoo.com>
2024-03-08 10:31:04 +0800

    Classifying Integers

--- array/exercise3.rb
+++ array/exercise3.rb
@@ -1,7 +1,33 @@
 # Develop a Ruby program that receives an array of ages and counts how many individuals
 # fall into each age group: "Child" (12 or younger), "Teenager" (13 to 19), and "Adult" (20 or older).
 
-ages
-child
-teenager
-adult
+def sort_by_age
+
+  puts "Input ages, press enter if none follows:"
+  ages = []
+  while true
+    input = gets.chomp
+    break if input.empty?
+    ages << input.to_i
+  end
+
+  child = []
+  teenager = []
+  adult = []
+
+  ages.each do |age|
+    if age >= 1 && age <= 12
+      child << age
+    elsif age >= 13 && age <= 19
+      teenager << age
+    elsif age >= 20
+      adult << age
+    end
+  end
+
+  puts "Number of children: #{child.length}"
+  puts  "Number of teenagers: #{teenager.length}"
+  puts  "Number of adults: #{adult.length}"
+end
+
+sort_by_age
--- array/exercise4.rb
+++ array/exercise4.rb
@@ -1,2 +1,25 @@
 # Write a Ruby script that takes an array of integers.
 # For each number, determine and store whether it's positive, negative, or zero.
+
+def classify_integer
+
+  puts "Input numbers, press enter if none follows:"
+  numbers = []
+  while true
+    input = gets.chomp
+    break if input.empty?
+    numbers << input.to_f
+  end
+
+  numbers.map do |number|
+    if number == 0
+      number = "Zero"
+    elsif number != 0 && number > 0
+      number = "Positive"
+    elsif number != 0 && number < 0
+      number = "Negative"
+    end
+  end
+end
+
+p classify_integer
