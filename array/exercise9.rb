f3eeb906ffdc2899a4eeea337e71bfd1eea28235
hannahmeg <hannah_arcega@yahoo.com>
2024-03-08 13:18:07 +0800

    Appending Classification to Integer

--- array/exercise9.rb
+++ array/exercise9.rb
@@ -3,3 +3,25 @@
 # If the number is positive, append "Positive" to the result; for negative numbers, append "Negative";
 # for zero, append "Zero."
 
+def classify_integer
+
+  puts "Input numbers, press enter if none follows:"
+  numbers = []
+  while true
+    input = gets.chomp
+    break if input.empty?
+    numbers << input.to_f
+  end
+
+  numbers.map do |number|
+    if number == 0
+      puts "#{number} Zero"
+    elsif number != 0 && number > 0
+      puts "#{number} Positive"
+    elsif number != 0 && number < 0
+      puts "#{number} Negative"
+    end
+  end
+end
+
+classify_integer
