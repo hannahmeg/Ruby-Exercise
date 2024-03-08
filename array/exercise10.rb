58d1969c44cef71812bfc9578021817b76c62b82
hannahmeg <hannah_arcega@yahoo.com>
2024-03-08 13:21:20 +0800

    Calculating Discounts

--- array/exercise10.rb
+++ array/exercise10.rb
@@ -3,3 +3,32 @@
 # a $10 discount if it's between $75 and $150, and no discount if it's below $75.
 # Store the discounted amounts in a new array.
 
+def discount
+
+  puts "Input amount, press enter if none follows:"
+  amounts = []
+  while true
+    input = gets.chomp
+    break if input.empty?
+    amounts << input.to_i
+  end
+
+  new_amounts = []
+
+  amounts.each do |amount|
+    if amount > 150
+      new_amount = (amount - (amount *= 0.2))
+      new_amounts.push new_amount
+
+    elsif amount >= 75 && amount <= 150
+      new_amount = (amount - (amount *= 0.10))
+      new_amounts.push new_amount
+
+    elsif amount < 75
+      new_amounts.push amount
+    end
+  end
+  puts "The discounted prices are: #{new_amounts}"
+end
+
+discount
