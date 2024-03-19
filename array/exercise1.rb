61bf34b12f6e7e9de54d15b920cb69f0f0a65b82
hannahmeg <hannah_arcega@yahoo.com>
2024-03-19 10:28:49 +0800

    Added Code Using While Loop

--- array/exercise1.rb
+++ array/exercise1.rb
@@ -3,30 +3,73 @@
 # "B" if it's between 80 and 89, "C" for scores between 70 and 79, and "D" for scores below 70.
 
 
-def classify_score
+# def classify_score
+#
+#   puts "Input grades, press enter if none follows:"
+#   scores = []
+#   while true
+#     input = gets.chomp
+#     break if input.empty?
+#     scores << input.to_i
+#   end
+#
+#   scores.map do |score|
+#     if score >= 90
+#       score = "A"
+#     elsif score >= 80 && score <= 89
+#       score = "B"
+#     elsif score >= 70 && score <= 79
+#       score = "C"
+#     elsif score < 70
+#       score = "D"
+#     end
+#   end
+# end
+#
+# p classify_score
 
-  puts "Input grades, press enter if none follows:"
-  scores = []
-  while true
-    input = gets.chomp
-    break if input.empty?
-    scores << input.to_i
-  end
 
-  scores.map do |score|
-    if score >= 90
-      score = "A"
-    elsif score >= 80 && score <= 89
-      score = "B"
-    elsif score >= 70 && score <= 79
-      score = "C"
-    elsif score < 70
-      score = "D"
-    end
-  end
+
+# while code example
+# names = %w[Mike John Mark Jeff Rex]
+# index = 0
+#
+# while index < names.size
+#   p "Name: #{names[index]} => Index: #{index}"
+#   index += 1
+# end
+
+
+
+# while code
+puts "Input grades, press enter if none follows:"
+
+scores = []
+while true
+  input = gets.chomp
+  break if input.empty?
+  scores << input.to_i
 end
 
-p classify_score
+index = 0
+while index < scores.size
+  score = scores[index]
+
+  if score >= 90;
+    score = "A"
+  elsif score >= 80 && score <= 89
+    score = "B"
+  elsif score >= 70 && score <= 79
+    score = "C"
+  elsif score < 70
+    score = "D"
+  end
+  p score
+  index += 1
+end
+
+
+
 
 
 
