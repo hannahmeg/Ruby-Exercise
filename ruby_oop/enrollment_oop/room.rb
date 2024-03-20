aa2ef0cfc0bda33adab3736925d57631a12d4a43
hannahmeg <hannah_arcega@yahoo.com>
2024-03-20 17:18:22 +0800

    Class Room

--- ruby_oop/enrollment_oop/room.rb
+++ ruby_oop/enrollment_oop/room.rb
@@ -7,3 +7,23 @@ class Room
   end
 end
 
+# Generate 10 Instances of Class Room
+rooms = []
+input_count = 0
+puts "Input Room Number and Room Name separated by comma. Press enter after each entry and repeat 10 times."
+while input_count != 10
+  input = gets.chomp
+  rooms << input.split(", ")
+  input_count += 1
+end
+
+
+i = 0
+while i < rooms.size
+  rooms.each do |room|
+    Room.new(room[0], room[1])
+    i += 1
+  end
+end
+
+p ObjectSpace.each_object(Room).to_a
