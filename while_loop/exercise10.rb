0d0643f96cdfe0c12c5d261e9fed4a283c58c1d8
hannahmeg <hannah_arcega@yahoo.com>
2024-03-20 13:24:52 +0800

    First Commit

---
+++ while_loop/exercise10.rb
@@ -0,0 +1,34 @@
+# Create a Ruby program that accepts an array of purchase amounts.
+# For each purchase, apply a $20 discount if the amount is greater than $150,
+# a $10 discount if it's between $75 and $150, and no discount if it's below $75.
+# Store the discounted amounts in a new array.
+
+#while code
+puts "Input amount, press enter if none follows:"
+amounts = []
+while true
+  input = gets.chomp
+  break if input.empty?
+  amounts << input.to_i
+end
+
+new_amounts = []
+index = 0
+while index < amounts.size
+  amount = amounts[index]
+
+  if amount > 150
+    new_amount = (amount - (amount *= 0.2))
+    new_amounts.push new_amount
+
+  elsif amount >= 75 && amount <= 150
+    new_amount = (amount - (amount *= 0.10))
+    new_amounts.push new_amount
+
+  elsif amount < 75
+    new_amounts.push amount
+  end
+  index += 1
+end
+
+puts "The discounted prices are: #{new_amounts}"
