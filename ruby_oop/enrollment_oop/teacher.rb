c5e39c3322f67dbaf85966f61b32b66a52352f55
hannahmeg <hannah_arcega@yahoo.com>
2024-03-20 16:55:45 +0800

    Class Teacher

---
+++ ruby_oop/enrollment_oop/teacher.rb
@@ -0,0 +1,11 @@
+class Teacher
+  attr_accessor :id, :name, :birth_date, :specialty, :email
+  def initialize(id, name)
+    @id = id
+    @name = name
+    @birth_date = birth_date
+    @specialty = specialty
+    @email = email
+  end
+end
+
