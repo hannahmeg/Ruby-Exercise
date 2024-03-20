6d19c546b38c50da0231ae3de473192fcb791754
hannahmeg <hannah_arcega@yahoo.com>
2024-03-20 16:53:08 +0800

    Index

---
+++ ruby_oop/enrollment_oop/index.rb
@@ -0,0 +1,19 @@
+# Course  # attributes:  id, name
+# Subject # attributes: id, name
+# Teacher # attributes: id, name, birth_date, specialty, email
+# Student # attributes: id, name, birth_date, course_id, parent
+# Room    # attributes: id, name
+#
+# 1. For each class, create a class method that will generate 10 instances of course, subject, teacher and students, rooms
+# 2. Create a script that will show all the students & courses data
+# 3. allow the user to be able to pick the student and assign to a specific course
+# Create a new class SubjectStudents
+# 4. SubjectStudents => attributes: subject_id, student_id, teacher_id, day
+# 5. Display the list of students, teachers and subjects
+# 6. Allow the user to create SubjectStudents by choosing the following students, teachers and subjects
+
+require_relative 'course'
+require_relative 'subject'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'room'
