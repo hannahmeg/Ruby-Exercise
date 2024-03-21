2c909cb5fd01e59bee0ac02e2a0da816eba7a0d6
hannahmeg <hannah_arcega@yahoo.com>
2024-03-21 13:32:24 +0800

    Course

--- ruby_oop/enrollment_oop/course.rb
+++ ruby_oop/enrollment_oop/course.rb
@@ -1,35 +1,57 @@
 class Course
   attr_accessor :id, :name
+  @@course_records = []
 
   def initialize(id, name)
     @id = id
     @name = name
   end
-end
 
+  def self.list
+    puts "List of Courses:"
+    @@course_records.each do |course|
+      puts "#{course.id} - #{course.name}"
+    end
+    # p ObjectSpace.each_object(Course).to_a
+  end
 
-# Generate 10 Instances of Class Course
-courses = []
-input_count = 0
-puts "Input Course ID and Course Name separated by comma. Press enter after each entry and repeat 10 times."
-while input_count != 10
-input = gets.chomp
-courses << input.split(", ")
-input_count += 1
-end
-
-
-i = 0
-while i < courses.size
-  courses.each do |course|
-  Course.new(course[0], course[1])
-  i += 1
+  def self.generate_samples
+    courses = [
+      { id: '001', name: 'Bachelor of Arts in Psychology' },
+      { id: '002', name: 'Bachelor of Science in Computer Science' },
+      { id: '003', name: 'Bachelor of Business Administration' },
+      { id: '004', name: 'Bachelor of Science in Nursing' },
+      { id: '005', name: 'Bachelor of Fine Arts in Graphic Design' },
+      { id: '006', name: 'Bachelor of Science in Mechanical Engineering' },
+      { id: '007', name: 'Bachelor of Arts in English Literature' },
+      { id: '008', name: 'Bachelor of Science in Biology' },
+      { id: '009', name: 'Bachelor of Architecture' },
+      { id: '010', name: 'Bachelor of Arts in History' }
+    ]
+    courses.each do |element|
+      course = Course.new(element[:id], element[:name])
+      @@course_records << course
+    end
   end
 end
 
-p ObjectSpace.each_object(Course).to_a
-
-
+# Generate 10 Instances of Class Course (with user input)
+# courses = []
+# input_count = 0
+# puts "Input Course ID and Course Name separated by comma. Press enter after each entry and repeat 10 times."
+# while input_count != 10
+# input = gets.chomp
+# courses << input.split(", ")
+# input_count += 1
+# end
+#
+# i = 0
+# while i < courses.size
+#   courses.each do |course|
+#   Course.new(course[0], course[1])
+#   i += 1
+#   end
+# end
 
 
 
