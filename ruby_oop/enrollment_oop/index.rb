d960f2ec9d92200dede22b41efa6e5695a8e2fca
hannahmeg <hannah_arcega@yahoo.com>
2024-03-21 14:41:12 +0800

    Index

--- ruby_oop/enrollment_oop/index.rb
+++ ruby_oop/enrollment_oop/index.rb
@@ -8,19 +8,30 @@
 # 2. Create a script that will show all the students & courses data
 # 3. allow the user to be able to pick the student and assign to a specific course
 # 4. Create a new class SubjectStudents
-# 5. SubjectStudents => attributes: subject_id, student_id, teacher_id, day
+# 5. SubjectStudents => attributes: subject_id, student_id, teacher_id, day (monday, tuesday)
 # 6. Display the list of students, teachers and subjects
 # 7. Allow the user to create SubjectStudents by choosing the following students, teachers and subjects
 
 require_relative 'course'
 require_relative 'subject'
+require_relative 'room'
+require_relative 'student'
+require_relative 'teacher'
 
-Course.generate_samples
-Course.list
+# Course.generate_samples
+# Course.list
+#
+# Subject.generate_samples
+# Subject.list
+#
+# Room.generate_samples
+# Room.list
+#
+# Student.generate_samples
+# Student.list
 
-Subject.generate_samples
-Subject.list
-
+Teacher.generate_samples
+Teacher.list
 
 # ENROLLSTUDENT:
 #
@@ -45,15 +56,4 @@ Subject.list
 # do you want to save? (y/n)?
 #
 # @@studnet_records <<
-
-# class SubjectStudents
-#   attr_accessor :subject_id, :student_id, :teacher_id, :day
-#
-#   def initialize(subject_id, student_id, teacher_id, day))
-#   @subject_id = subject_id
-#   @student_id = student_id
-#   @teacher_id = teacher_id
-#   @day = day #date today
-#   end
-# end
 
